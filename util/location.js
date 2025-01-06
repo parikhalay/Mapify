b8e84a0d0874bb79d3ac742cbe047c6ee62a77db
parikhalay <parikh.alay58@gmail.com>
2025-01-06 13:33:21 -0500

    Added Backend

---
+++ util/location.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+const getCoordsForAddress = async (address) => {
+    
+    return {
+        lat: 40.7484405,
+        lng: -73.9882393
+    };
+
+
+}
+
+module.exports = getCoordsForAddress;
